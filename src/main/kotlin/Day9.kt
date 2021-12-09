cb97f98f497e93d25a341641c433ae18c094c680
tiesjr <ties@jdriven.com>
2021-12-09 17:37:16 +0100

    day 9 :)

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,6 +1,8 @@
 import kotlin.system.measureTimeMillis
 
 class Day9 {
+    val MAX_CELL = 9999
+
     fun day9() {
         val fileContent = Day9::class.java.getResource("/day9.txt").readText()
         val cells: List<Cell> =
@@ -16,24 +18,23 @@ class Day9 {
         println(part1Cells.sumOf { it.value + 1 })
 
         val time = measureTimeMillis {
-            //Absolutely horrible implementation that takes forever because it goes through a huge list multiple times and
-            //creates more huge lists in the process.. but it works.. didnt have much time today though :(
             val cleanMap = cells.map {
                 if (it.value != 9) {
                     Cell(0, it.x, it.y)
                 } else {
-                    Cell(9999, it.x, it.y)
+                    Cell(MAX_CELL, it.x, it.y)
                 }
             }
 
             val grid: MutableMap<Int, MutableMap<Int, Cell>> = cleanMap.groupBy { it.x }.mapValues { it.value.groupBy { it.y } }.mapValues { it.value.mapValues { it.value.first() } as MutableMap } as MutableMap
-            val maxX = grid.keys.maxOrNull()!!
-            val maxY = grid.flatMap { it.value.keys }.maxOrNull()!!
-            for(basinNumber in 1..(maxX * maxY)){
-                fillBasin(grid, basinNumber)
+
+            var basinNumber = 1
+            part1Cells.forEach{
+                fillBasin(it.x, it.y, grid, basinNumber++)
             }
+
             println(grid.values.flatMap { it.values }
-                .filterNot { it.value == 9999 }
+                .filterNot { it.value == MAX_CELL }
                 .groupBy { it.value }
                 .mapValues { it.value.size }
                 .map { it.value }
@@ -45,50 +46,15 @@ class Day9 {
         println("took: ${time}ms")
     }
 
-    fun print(grid: MutableMap<Int, MutableMap<Int, Cell>>) : String{
-        val maxX = grid.keys.maxOrNull()!!
-        val maxY = grid.flatMap { it.value.keys }.maxOrNull()!!
-        return IntRange(0, maxY).map { y ->
-            IntRange(0, maxX).map { x ->
-                if(grid[x]?.get(y)?.value == 9999){
-                    "***"
-                } else {
-                    "${grid[x]?.get(y)?.value!!}".padEnd(3)
-                }
-            }.joinToString(separator = "")
-        }.joinToString(separator = "\n")
+    fun fillBasin(x : Int, y : Int, grid : MutableMap<Int, MutableMap<Int, Cell>>, basinNumber: Int){
+        grid[x]?.put( y, grid[x]?.get(y)?.copy(value = basinNumber)!!)
+        fillNeighbours(x, y,basinNumber,grid)
     }
 
-    fun findStartPoint(grid : MutableMap<Int, MutableMap<Int, Cell>>) :Cell? {
-        val maxX = grid.keys.maxOrNull()!!
-        val maxY = grid.flatMap { it.value.keys }.maxOrNull()!!
-
-        for( y in 0..maxY) {
-            for (x in 0..maxX) {
-                if(grid[x]?.get(y)?.value!! == 0){
-                    return grid[x]?.get(y)!!
-                }
-            }
-        }
-        return null
-    }
-
-    fun fillBasin(grid : MutableMap<Int, MutableMap<Int, Cell>>, basinNumber: Int){
-        val startPoint = findStartPoint(grid)
-        if(startPoint == null){
-            return
-        }
-        grid[startPoint.x]?.put(startPoint.y,startPoint.copy(value = basinNumber))
-        fill(startPoint.x,startPoint.y,basinNumber,grid)
-    }
-    fun fill(x : Int, y : Int, basinNumber: Int, grid : MutableMap<Int, MutableMap<Int, Cell>>){
-        val neighbours = getNeighbours(x,y,grid)
-        if(neighbours.isEmpty()){
-            return
-        }
-        neighbours.forEach {
+    fun fillNeighbours(x : Int, y : Int, basinNumber: Int, grid : MutableMap<Int, MutableMap<Int, Cell>>){
+        getNeighbours(x,y,grid).forEach {
             grid[it.x]?.put(it.y, it.copy(value = basinNumber))
-            fill(it.x, it.y, basinNumber, grid)
+            fillNeighbours(it.x, it.y, basinNumber, grid)
         }
     }
 
@@ -99,4 +65,18 @@ class Day9 {
     }
 
     data class Cell(val value: Int, val x: Int, val y: Int)
+
+    fun print(grid: MutableMap<Int, MutableMap<Int, Cell>>) : String{
+        val maxX = grid.keys.maxOrNull()!!
+        val maxY = grid.flatMap { it.value.keys }.maxOrNull()!!
+        return IntRange(0, maxY).map { y ->
+            IntRange(0, maxX).map { x ->
+                if(grid[x]?.get(y)?.value == MAX_CELL){
+                    "***"
+                } else {
+                    "${grid[x]?.get(y)?.value!!}".padEnd(3)
+                }
+            }.joinToString(separator = "")
+        }.joinToString(separator = "\n")
+    }
 }
