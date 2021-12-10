c1d87c0ccfda5e1e4be308ca528dd6b3b8b11963
tiesjr <ties@jdriven.com>
2021-12-10 12:13:12 +0100

    day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -4,36 +4,59 @@ class Day10 {
 
     val POINTS_MAP = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
 
-    fun day10(){
-        val fileContent = Day1::class.java.getResource("/day10.txt").readText()
+    val POINTS_MAP_PART2 = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+
+
+    fun day10() {
+        val fileContent = Day10::class.java.getResource("/day10.txt").readText()
         val lines = fileContent.lines()
 
         //part1
-        println(lines.map {
-            getFirstIllegalChar(it)
-        }.filterNotNull()
-            .map { POINTS_MAP[it]!! }.sum())
+        println(
+            lines.map { getFirstIllegalChar(it) }
+                .filterNotNull()
+                .map { POINTS_MAP[it]!! }.sum()
+        )
 
-
-        lines.filter {
+        //part2
+        val scores = lines.filter {
             getFirstIllegalChar(it) == null
-        }
+        }.map { getEnding(it) }
+            .map { it.toCharArray().map { END_CHAR_MAP[it]!! }.reversed().joinToString(separator = "") }
+            .map { calculatePoints(it) }
+            .sorted()
+
+        println(scores[scores.size / 2])
     }
 
-    fun getFirstIllegalChar(s : String) : Char? {
-        if(s.isEmpty()) {
+    fun calculatePoints(s: String) =s.toCharArray()
+        .map { POINTS_MAP_PART2[it]!! }
+        .fold(0L) { acc, e ->
+            val newAcc = acc * 5
+            newAcc + e
+        }
+
+    tailrec fun getEnding(s: String): String {
+        var st = s
+        END_CHAR_MAP.map { "${it.key}${it.value}" }.forEach {
+            st = st.replace(it, "")
+        }
+        if (s == st) {
+            return st
+        }
+        return getEnding(st)
+    }
+
+    tailrec fun getFirstIllegalChar(s: String): Char? {
+        if (s.isEmpty()) {
             return null
         }
         var st = s
-        END_CHAR_MAP.map { "${it.key}${it.value}" }.forEach{
-           st = st.replace(it, "")
+        END_CHAR_MAP.map { "${it.key}${it.value}" }.forEach {
+            st = st.replace(it, "")
         }
-        if(s == st){
-            val firstEndChar = st.toCharArray().firstOrNull(){END_CHAR_MAP.values.contains(it)}
-            if(firstEndChar == null){
-                return null
-            }
-            return firstEndChar
+        if (s == st) {
+            return st.toCharArray().firstOrNull() { END_CHAR_MAP.values.contains(it) }
         }
         return getFirstIllegalChar(st)
     }
