ffae4a9e51894422b092f5660ffe960ea3521586
tiesjr <ties@jdriven.com>
2021-12-13 09:07:09 +0100

    dat 12 part 2

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -7,13 +7,6 @@ class Day12 {
     fun day12() {
         val fileContent = Day12::class.java.getResource("/day12.txt").readText()
         val lines = fileContent.lines()
-//        start-A
-//        start-b
-//        A-c
-//        A-b
-//        b-d
-//        A-end
-//        b-end
 
         val caves = lines
             .flatMap { it.split("-") }
@@ -25,11 +18,11 @@ class Day12 {
             .forEach { c ->
                 val first = caves.first { it.id == c[0] }
                 val second = caves.first { it.id == c[1] }
-                if(first is Start || second is End){
+                if (first is Start || second is End) {
                     first.connections.add(second.id)
-                }else if(second is Start || first is End) {
+                } else if (second is Start || first is End) {
                     second.connections.add(first.id)
-                }else {
+                } else {
                     first.connections.add(second.id)
                     second.connections.add(first.id)
                 }
@@ -37,40 +30,73 @@ class Day12 {
 
             }
 
-//        println(caves)
-
         val start = caves.filterIsInstance<Start>().first()
-        val currentRoute = mutableListOf(start)
-        println(findAllPaths(start, currentRoute,caves).map {
-            it.map { it.id }.joinToString(separator = ", ")
-        }.size)
 
+        println(findAllPaths(start, mutableListOf(), caves).size)
+        println(findAllPaths2(start, mutableListOf(), caves).size)
     }
 
-    fun findAllPaths(node: Cave, currentRoute: List<Cave>, caves : List<Cave>): List<List<Cave>> {
-//        println("current: ${node.id}")
-        val last = currentRoute.last()
+    fun findAllPaths(node: Cave, currentRoute: List<Cave>, caves: List<Cave>): List<List<Cave>> {
         val paths = mutableListOf<List<Cave>>()
         if (node.connections.isEmpty() && node is SmallCave) {
-//            println("${node.id} has no connections, currentRoute: ${currentRoute.map { it.id }}")
-
+            val last = currentRoute.last()
             if (last is BigCave) {
-                paths.addAll(findAllPaths(last, currentRoute.plus(node),caves).map { listOf(node) + it })
+                paths.addAll(findAllPaths(last, currentRoute.plus(node), caves).map { listOf(node) + it })
                 return paths
             }
         }
-        for (nextNode in node.connections.map { c -> caves.first{ it.id == c}}) {
+        for (nextNode in node.connections.map { c -> caves.first { it.id == c } }) {
             if (!(nextNode is SmallCave && currentRoute.contains(nextNode))) {
                 if (nextNode is End) {
                     paths.add(mutableListOf(node, nextNode))
                 } else {
-                    paths.addAll(findAllPaths(nextNode, currentRoute.plus(node),caves).map { listOf(node) + it })
+                    paths.addAll(findAllPaths(nextNode, currentRoute.plus(node), caves).map { listOf(node) + it })
                 }
             }
         }
         return paths
     }
 
+
+    fun findAllPaths2(node: Cave, currentRoute: List<Cave>, caves: List<Cave>): List<List<Cave>> {
+        val paths = mutableListOf<List<Cave>>()
+        if (node.connections.isEmpty() && node is SmallCave) {
+            val last = currentRoute.last()
+            if (last is BigCave) {
+                paths.addAll(findAllPaths2(last, currentRoute.plus(node), caves).map { listOf(node) + it })
+                return paths
+            }
+        }
+        for (nextNode in node.connections.map { c -> caves.first { it.id == c } }) {
+            if (nextNode is SmallCave) {
+                if (canVisit(nextNode, currentRoute.plus(node))) {
+                    val nextPaths: List<List<Cave>> =
+                        findAllPaths2(nextNode, currentRoute.plus(node), caves).map { listOf(node) + it }
+                    paths.addAll(nextPaths)
+                }
+            } else {
+                if (nextNode is End) {
+                    if (node is SmallCave) {
+                        if (canVisit(nextNode, currentRoute.plus(node))) {
+                            paths.add(mutableListOf(node, nextNode))
+                        }
+                    } else {
+                        paths.add(mutableListOf(node, nextNode))
+                    }
+                } else {
+                    paths.addAll(findAllPaths2(nextNode, currentRoute.plus(node), caves).map { listOf(node) + it })
+                }
+            }
+        }
+        return paths
+    }
+
+    fun canVisit(node : Cave, currentRoute : List<Cave>) : Boolean {
+        val smallCaves = currentRoute.filterIsInstance<SmallCave>()
+        val hasNoDouble = smallCaves.toSet().size == smallCaves.size
+        val hasBeenVisited = smallCaves.contains(node)
+        return (!hasBeenVisited) || (hasBeenVisited && hasNoDouble)
+    }
 
     fun toCave(it: String) = when (it) {
         "start" -> Start(it, mutableSetOf())
@@ -84,8 +110,6 @@ class Day12 {
         abstract val connections: MutableSet<String>
     }
 
-
-    data class Connection(val from: Cave, val to: Cave)
     data class Start(override val id: String, override val connections: MutableSet<String>) : Cave()
     data class End(override val id: String, override val connections: MutableSet<String>) : Cave()
     data class SmallCave(override val id: String, override val connections: MutableSet<String>) : Cave()
